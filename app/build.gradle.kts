2efd69ce5453a942d8725452d9e4e4db0bd374d9
Joe Beaulieu <64218687+Joe-Beaulieu-Dev@users.noreply.github.com>
2024-09-29 15:09:23 -0400

    Add DataStore to gradle file. Move Material Icon dependency to toml file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("androidx.compose.material:material-icons-extended:1.6.6")
+    implementation(libs.androidx.compose.material.icons)
 
     // Lifecycle
     implementation(libs.androidx.lifecycle.process)
@@ -75,6 +75,9 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    // DataStore
+    implementation(libs.androidx.datastore)
 
     testImplementation(libs.junit)
 
