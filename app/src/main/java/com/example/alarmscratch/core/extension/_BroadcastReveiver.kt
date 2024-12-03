3b44ec6d226e6da4d4c9f6ca914bf433c114b77c
Joe Beaulieu <64218687+Joe-Beaulieu-Dev@users.noreply.github.com>
2024-12-03 18:04:52 -0500

    Move BroadcastReceiver database coroutines to Application bound scope

---
+++ app/src/main/java/com/example/alarmscratch/core/extension/_BroadcastReveiver.kt
@@ -0,0 +1,21 @@
+package com.example.alarmscratch.core.extension
+
+import android.content.BroadcastReceiver
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+fun BroadcastReceiver.doAsync(
+    applicationScope: CoroutineScope,
+    dispatcher: CoroutineDispatcher,
+    block: suspend () -> Unit
+) {
+    val pendingResult = goAsync()
+    applicationScope.launch(dispatcher) {
+        try {
+            block()
+        } finally {
+            pendingResult.finish()
+        }
+    }
+}
