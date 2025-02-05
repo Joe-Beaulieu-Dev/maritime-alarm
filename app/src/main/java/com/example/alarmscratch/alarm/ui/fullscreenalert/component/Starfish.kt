78df492c082668e8f716c56e1f38ddeb778767e9
Joe Beaulieu <64218687+Joe-Beaulieu-Dev@users.noreply.github.com>
2025-02-05 16:39:33 -0500

    Convert straight lines to bezier curves on Starfish arms

--- app/src/main/java/com/example/alarmscratch/alarm/ui/fullscreenalert/component/Starfish.kt
+++ app/src/main/java/com/example/alarmscratch/alarm/ui/fullscreenalert/component/Starfish.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.alarmscratch.core.ui.theme.AlarmScratchTheme
-import com.example.alarmscratch.core.ui.theme.DarkLavaRed
+import com.example.alarmscratch.core.ui.theme.StarfishBase
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
@@ -22,45 +22,185 @@ fun Starfish(
     starfishSize: Dp,
     modifier: Modifier = Modifier
 ) {
-    val radiusPixels = with(LocalDensity.current) { (starfishSize / 2).toPx() }
+    // General Values
+    val localDensity = LocalDensity.current
+    val outerCircleRadius = with(localDensity) { (starfishSize / 2).toPx() }
+    val innerCircleRadius = outerCircleRadius / 2.5f
+    val armBulge = with(localDensity) { (starfishSize / 10).toPx() }
 
     // Outer Points
-    val rightArmOuterPoint = calculateOuterPoint(radiusPixels, 0, radiusPixels)
-    val headOuterPoint = calculateOuterPoint(radiusPixels, 1, radiusPixels)
-    val leftArmOuterPoint = calculateOuterPoint(radiusPixels, 2, radiusPixels)
-    val leftLegOuterPoint = calculateOuterPoint(radiusPixels, 3, radiusPixels)
-    val rightLegOuterPoint = calculateOuterPoint(radiusPixels, 4, radiusPixels)
+    val rightArmOuterPoint = calculateOuterPoint(outerCircleRadius, 0, outerCircleRadius)
+    val headOuterPoint = calculateOuterPoint(outerCircleRadius, 1, outerCircleRadius)
+    val leftArmOuterPoint = calculateOuterPoint(outerCircleRadius, 2, outerCircleRadius)
+    val leftLegOuterPoint = calculateOuterPoint(outerCircleRadius, 3, outerCircleRadius)
+    val rightLegOuterPoint = calculateOuterPoint(outerCircleRadius, 4, outerCircleRadius)
 
     // Inner Points
-    val rightNeckInnerPoint = calculateInnerPoint(radiusPixels / 2.5f, 0, radiusPixels)
-    val leftNeckInnerPoint = calculateInnerPoint(radiusPixels / 2.5f, 1, radiusPixels)
-    val leftArmpitInnerPoint = calculateInnerPoint(radiusPixels / 2.5f, 2, radiusPixels)
-    val bottomInnerPoint = calculateInnerPoint(radiusPixels / 2.5f, 3, radiusPixels)
-    val rightArmpitInnerPoint = calculateInnerPoint(radiusPixels / 2.5f, 4, radiusPixels)
+    val rightNeckInnerPoint = calculateInnerPoint(innerCircleRadius, 0, outerCircleRadius)
+    val leftNeckInnerPoint = calculateInnerPoint(innerCircleRadius, 1, outerCircleRadius)
+    val leftArmpitInnerPoint = calculateInnerPoint(innerCircleRadius, 2, outerCircleRadius)
+    val bottomInnerPoint = calculateInnerPoint(innerCircleRadius, 3, outerCircleRadius)
+    val rightArmpitInnerPoint = calculateInnerPoint(innerCircleRadius, 4, outerCircleRadius)
 
     Box(
         modifier = modifier
             .size(starfishSize)
             .rotate(180f)
             .drawBehind {
-                val starfish = Path().apply {
-                    // Top Half
+                val starfishBase = Path().apply {
+                    /*
+                     * Top Half
+                     */
+
+                    // Start: Right Arm Outer Point
                     moveTo(rightArmOuterPoint.first, rightArmOuterPoint.second)
-                    lineTo(rightNeckInnerPoint.first, rightNeckInnerPoint.second)
-                    lineTo(headOuterPoint.first, headOuterPoint.second)
-                    lineTo(leftNeckInnerPoint.first, leftNeckInnerPoint.second)
-                    lineTo(leftArmOuterPoint.first, leftArmOuterPoint.second)
 
-                    // Bottom Half
-                    lineTo(leftArmpitInnerPoint.first, leftArmpitInnerPoint.second)
-                    lineTo(leftLegOuterPoint.first, leftLegOuterPoint.second)
-                    lineTo(bottomInnerPoint.first, bottomInnerPoint.second)
-                    lineTo(rightLegOuterPoint.first, rightLegOuterPoint.second)
-                    lineTo(rightArmpitInnerPoint.first, rightArmpitInnerPoint.second)
+                    // Move: Right Arm Outer Point -> Right Neck Inner Point
+                    val rightArmToRightNeckCenterPointX =
+                        ((rightArmOuterPoint.first - rightNeckInnerPoint.first) / 2) + rightNeckInnerPoint.first
+                    val rightArmToRightNeckCenterPointY = rightArmOuterPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = rightArmToRightNeckCenterPointX,
+                        y1 = rightArmToRightNeckCenterPointY + armBulge,
+                        // End point
+                        x2 = rightNeckInnerPoint.first,
+                        y2 = rightNeckInnerPoint.second
+                    )
 
+                    // Move: Right Neck Inner Point -> Head Outer Point
+                    val rightNeckToHeadCenterPointX =
+                        ((rightNeckInnerPoint.first - headOuterPoint.first) / 2) + headOuterPoint.first
+                    val rightNeckToHeadCenterPointY =
+                        ((headOuterPoint.second - rightNeckInnerPoint.second) / 2) + rightNeckInnerPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = rightNeckToHeadCenterPointX + armBulge,
+                        y1 = rightNeckToHeadCenterPointY,
+                        // End point
+                        x2 = headOuterPoint.first,
+                        y2 = headOuterPoint.second
+                    )
+
+                    // Move: Head Outer Point -> Left Neck Inner Point
+                    val headToLeftNeckCenterPointX =
+                        ((headOuterPoint.first - leftNeckInnerPoint.first) / 2) + leftNeckInnerPoint.first
+                    val headToLeftNeckCenterPointY =
+                        ((headOuterPoint.second - leftNeckInnerPoint.second) / 2) + leftNeckInnerPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = headToLeftNeckCenterPointX - armBulge,
+                        y1 = headToLeftNeckCenterPointY,
+                        // End point
+                        x2 = leftNeckInnerPoint.first,
+                        y2 = leftNeckInnerPoint.second
+                    )
+
+                    // Move: Left Neck Inner Point -> Left Arm Outer Point
+                    val leftNeckToLeftArmCenterPointX =
+                        ((leftNeckInnerPoint.first - leftArmOuterPoint.first) / 2) + leftArmOuterPoint.first
+                    val leftNeckToLeftArmCenterPointY = leftNeckInnerPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = leftNeckToLeftArmCenterPointX,
+                        y1 = leftNeckToLeftArmCenterPointY + armBulge,
+                        // End point
+                        x2 = leftArmOuterPoint.first,
+                        y2 = leftArmOuterPoint.second
+                    )
+
+                    /*
+                     * Bottom Half
+                     */
+
+                    // Move: Left Arm Outer Point -> Left Armpit Inner Point
+                    val leftArmToLeftArmpitCenterPointX =
+                        ((leftArmpitInnerPoint.first - leftArmOuterPoint.first) / 2) + leftArmOuterPoint.first
+                    val leftArmToLeftArmpitCenterPointY =
+                        ((leftArmOuterPoint.second - leftArmpitInnerPoint.second) / 2) + leftArmpitInnerPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = leftArmToLeftArmpitCenterPointX,
+                        y1 = leftArmToLeftArmpitCenterPointY - armBulge,
+                        // End point
+                        x2 = leftArmpitInnerPoint.first,
+                        y2 = leftArmpitInnerPoint.second
+                    )
+
+                    // Move: Left Armpit Inner Point -> Left Leg Outer Point
+                    val leftArmpitToLeftLegCenterPointX =
+                        ((leftArmpitInnerPoint.first - leftLegOuterPoint.first) / 2) + leftLegOuterPoint.first
+                    val leftArmpitToLeftLegCenterPointY =
+                        ((leftArmpitInnerPoint.second - leftLegOuterPoint.second) / 2) + leftLegOuterPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = leftArmpitToLeftLegCenterPointX - armBulge,
+                        y1 = leftArmpitToLeftLegCenterPointY + armBulge,
+                        // End point
+                        x2 = leftLegOuterPoint.first,
+                        y2 = leftLegOuterPoint.second
+                    )
+
+                    // Move: Left Leg Outer Point -> Bottom Inner Point
+                    val leftLegToBottomCenterPointX =
+                        ((bottomInnerPoint.first - leftLegOuterPoint.first) / 2) + leftLegOuterPoint.first
+                    val leftLegToBottomCenterPointY =
+                        ((bottomInnerPoint.second - leftLegOuterPoint.second) / 2) + leftLegOuterPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = leftLegToBottomCenterPointX + armBulge,
+                        y1 = leftLegToBottomCenterPointY - armBulge,
+                        // End point
+                        x2 = bottomInnerPoint.first,
+                        y2 = bottomInnerPoint.second
+                    )
+
+                    // Move: Bottom Inner Point -> Right Leg Outer Point
+                    val bottomToRightLegCenterPointX =
+                        ((rightLegOuterPoint.first - bottomInnerPoint.first) / 2) + bottomInnerPoint.first
+                    val bottomToRightLegCenterPointY =
+                        ((bottomInnerPoint.second - rightLegOuterPoint.second) / 2) + rightLegOuterPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = bottomToRightLegCenterPointX - armBulge,
+                        y1 = bottomToRightLegCenterPointY - armBulge,
+                        // End point
+                        x2 = rightLegOuterPoint.first,
+                        y2 = rightLegOuterPoint.second
+                    )
+
+                    // Move: Right Leg Outer Point -> Right Armpit Inner Point
+                    val rightLegToRightArmpitCenterPointX =
+                        ((rightLegOuterPoint.first - rightArmpitInnerPoint.first) / 2) + rightArmpitInnerPoint.first
+                    val rightLegToRightArmpitCenterPointY =
+                        ((rightArmpitInnerPoint.second - rightLegOuterPoint.second) / 2) + rightLegOuterPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = rightLegToRightArmpitCenterPointX + armBulge,
+                        y1 = rightLegToRightArmpitCenterPointY + armBulge,
+                        // End point
+                        x2 = rightArmpitInnerPoint.first,
+                        y2 = rightArmpitInnerPoint.second
+                    )
+
+                    // Move: Right Armpit Inner Point -> Right Arm Outer Point
+                    val rightArmpitToRightArmCenterPointX =
+                        ((rightArmOuterPoint.first - rightArmpitInnerPoint.first) / 2) + rightArmpitInnerPoint.first
+                    val rightArmpitToRightArmCenterPointY =
+                        ((rightArmOuterPoint.second - rightArmpitInnerPoint.second) / 2) + rightArmpitInnerPoint.second
+                    quadraticTo(
+                        // Bezier tip
+                        x1 = rightArmpitToRightArmCenterPointX,
+                        y1 = rightArmpitToRightArmCenterPointY - armBulge,
+                        // End point
+                        x2 = rightArmOuterPoint.first,
+                        y2 = rightArmOuterPoint.second
+                    )
+
+                    // Close for good measure
                     close()
                 }
-                drawPath(path = starfish, color = DarkLavaRed)
+                drawPath(path = starfishBase, color = StarfishBase)
             }
     )
 }
@@ -90,7 +230,7 @@ private fun calculateInnerPoint(radiusPixels: Float, multiplier: Int, offset: Fl
 private fun degreesToRadians(degrees: Float): Float = (degrees * (PI / 180)).toFloat()
 
 /*
- * Preview
+ * Previews
  */
 
 @Preview(
@@ -101,6 +241,19 @@ private fun degreesToRadians(degrees: Float): Float = (degrees * (PI / 180)).toF
 )
 @Composable
 private fun StarfishPreview() {
+    AlarmScratchTheme {
+        Starfish(300.dp)
+    }
+}
+
+@Preview(
+    widthDp = 300,
+    heightDp = 300,
+    showBackground = true,
+    backgroundColor = 0xFFffe3a0
+)
+@Composable
+private fun StarfishOnWetSandPreview() {
     AlarmScratchTheme {
         Starfish(300.dp)
     }
