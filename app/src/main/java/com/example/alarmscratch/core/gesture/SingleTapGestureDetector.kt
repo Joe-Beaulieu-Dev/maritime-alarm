3918dabdcc21a476304033dca0603053b0406fb3
Joe Beaulieu <64218687+Joe-Beaulieu-Dev@users.noreply.github.com>
2025-02-14 20:35:37 -0500

    Add Ripple to LongPressButton

--- app/src/main/java/com/example/alarmscratch/core/gesture/SingleTapGestureDetector.kt
+++ app/src/main/java/com/example/alarmscratch/core/gesture/SingleTapGestureDetector.kt
@@ -3,6 +3,8 @@ package com.example.alarmscratch.core.gesture
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.ui.input.pointer.AwaitPointerEventScope
 import androidx.compose.ui.input.pointer.PointerEventTimeoutCancellationException
 import androidx.compose.ui.input.pointer.PointerInputChange
@@ -10,6 +12,7 @@ import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.util.fastAny
 import androidx.compose.ui.util.fastForEach
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 /**
  * Detect and react to single tap gestures. Gestures include: [onPressStart], [onShortPress], and [onLongPress].
@@ -35,13 +38,20 @@ suspend fun PointerInputScope.detectSingleTapGestures(
     longPressTimeout: Long,
     onPressStart: (() -> Unit)? = null,
     onShortPress: (() -> Unit)? = null,
-    onLongPress: (() -> Unit)? = null
+    onLongPress: (() -> Unit)? = null,
+    interactionSource: MutableInteractionSource
 ) = coroutineScope {
     awaitEachGesture {
         // Consume the initial down event and invoke onPressStart()
         val down = awaitFirstDown()
         down.consume()
         onPressStart?.invoke()
+
+        // Start Ripple from the down event position
+        val ripplePress = PressInteraction.Press(down.position)
+        launch {
+            interactionSource.emit(ripplePress)
+        }
 
         // Wait for the next up event and consume it. If it takes longer than longPressTimeout
         // then a PointerEventTimeoutCancellationException is thrown and caught, onLongPress() is immediately
@@ -70,6 +80,11 @@ suspend fun PointerInputScope.detectSingleTapGestures(
         if (upOrCancel != null) {
             onShortPress?.invoke()
         }
+
+        // Release Ripple
+        launch {
+            releaseOrCancel(interactionSource, ripplePress, upOrCancel)
+        }
     }
 }
 
@@ -82,4 +97,25 @@ private suspend fun AwaitPointerEventScope.consumeUntilUp() {
         val event = awaitPointerEvent()
         event.changes.fastForEach { it.consume() }
     } while (event.changes.fastAny { it.pressed })
+}
+
+/**
+ * Emit either PressInteraction.Cancel or PressInteraction.Release from the given [interactionSource]
+ * based on whether [upOrCancel] is null or non-null, respectively.
+ *
+ * @param interactionSource MutableInteractionSource to emit the PressInteraction from
+ * @param downPress the source Press interaction that is being either released or cancelled
+ * @param upOrCancel PointerInputChange being used to determine if the Press is being released or cancelled
+ */
+private suspend fun releaseOrCancel(
+    interactionSource: MutableInteractionSource,
+    downPress: PressInteraction.Press,
+    upOrCancel: PointerInputChange?
+) {
+    when (upOrCancel) {
+        null ->
+            interactionSource.emit(PressInteraction.Cancel(downPress))
+        else ->
+            interactionSource.emit(PressInteraction.Release(downPress))
+    }
 }
--- app/src/main/java/com/example/alarmscratch/core/ui/shared/LongPressButton.kt
+++ app/src/main/java/com/example/alarmscratch/core/ui/shared/LongPressButton.kt
@@ -1,5 +1,7 @@
 package com.example.alarmscratch.core.ui.shared
 
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,9 +15,12 @@ import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.semantics.Role
@@ -32,32 +37,35 @@ fun LongPressButton(
     onPressStart: () -> Unit,
     onShortPress: () -> Unit,
     onLongPress: () -> Unit,
+    modifier: Modifier = Modifier,
     enabled: Boolean = true,
     shape: Shape = ButtonDefaults.shape,
     colors: ButtonColors = ButtonDefaults.buttonColors(),
     contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
-    modifier: Modifier = Modifier,
     content: @Composable RowScope.() -> Unit
 ) {
     val containerColor = if (enabled) colors.containerColor else colors.disabledContainerColor
     val contentColor = if (enabled) colors.contentColor else colors.disabledContentColor
+    val interactionSource = remember { MutableInteractionSource() }
 
     Surface(
-        shape = shape,
         color = containerColor,
         contentColor = contentColor,
         modifier = modifier
             .semantics { role = Role.Button }
+            .clip(shape)
             .pointerInput(enabled) {
                 if (enabled) {
                     detectSingleTapGestures(
                         longPressTimeout = longPressTimeout,
                         onPressStart = onPressStart,
                         onShortPress = onShortPress,
-                        onLongPress = onLongPress
+                        onLongPress = onLongPress,
+                        interactionSource = interactionSource
                     )
                 }
             }
+            .indication(interactionSource, ripple())
     ) {
         Row(
             horizontalArrangement = Arrangement.Center,
@@ -91,7 +99,7 @@ private fun LongPressButtonPreview() {
                 .height(200.dp)
         ) {
             LongPressButton(
-                longPressTimeout = 3000,
+                longPressTimeout = 3000L,
                 onPressStart = {},
                 onShortPress = {},
                 onLongPress = {}
