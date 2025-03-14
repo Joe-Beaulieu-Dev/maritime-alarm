923b0d4723e428177f0091da29bfaa047753cb9f
Joe Beaulieu <64218687+Joe-Beaulieu-Dev@users.noreply.github.com>
2025-03-13 20:54:46 -0400

    Change Alarm NotificationChannel strings

--- app/src/main/java/com/example/alarmscratch/core/ui/notificationcheck/AppNotificationChannel.kt
+++ app/src/main/java/com/example/alarmscratch/core/ui/notificationcheck/AppNotificationChannel.kt
@@ -14,7 +14,7 @@ sealed class AppNotificationChannel(
 ) {
     // TODO: Revisit these strings
     data object Alarm : AppNotificationChannel(
-        id = AlarmNotification.CHANNEL_ID_ALARM_NOTIFICATION,
+        id = AlarmNotification.ALARM_NOTIFICATION_CHANNEL_ID,
         name = R.string.permission_channel_alarm_name,
         importance = NotificationManager.IMPORTANCE_HIGH,
         description = R.string.permission_channel_alarm_desc,
