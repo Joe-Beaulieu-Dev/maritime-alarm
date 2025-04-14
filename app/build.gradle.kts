e4f34f3c240e84d811c92dff6ea333a7f22f572c
Joe Beaulieu <64218687+Joe-Beaulieu-Dev@users.noreply.github.com>
2025-04-14 16:14:55 -0400

    Update both compileSdk and targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.joebsource.lavalarm"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.joebsource.lavalarm"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
